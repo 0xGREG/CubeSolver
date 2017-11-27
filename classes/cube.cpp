8a0ae5cb2d850b15aab25a22454d39eb42b6c1b7
Grzegorz Zielinski <07greg98@gmail.com>
2017-11-27 00:15:12 +0000

    added manual and solved state checker

--- classes/cube.cpp
+++ classes/cube.cpp
@@ -1,4 +1,6 @@
 #include "cube.h"
+#include <iostream>
+using namespace std;
 
 cube::cube(int _size){
     size = _size;
@@ -7,6 +9,42 @@ cube::cube(int _size){
         for (int j = 0; j < 9; j++)
         {
             tiles[i][j] = 0;
+        }
+    }
+    correct = false;
+    solved = false;
+}
+
+void cube::updateIsSolved(){
+    for (int i = 0; i < 6; i++)
+    {
+        bool sideSolved = true;
+        short color = tiles[i][0];
+        for (int j = 1; j < 9; j++)
+        {
+            if (tiles[i][j] != color)
+            {
+                sideSolved = false;
+                break;
+            }
+        }
+        if (!sideSolved)
+        {
+            solved = false;
+            break;
+        }
+    }
+    solved = true;
+}
+
+bool cube::setTiles(string order)
+{
+    short newNet[6][9];
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            newNet[i][j] = -1;
         }
     }
 }
--- classes/cube.h
+++ classes/cube.h
@@ -1,14 +1,28 @@
 #ifndef CUBE_H
 #define CUBE_H
 
+#include <iostream>
+using namespace std;
+
 class cube
 {
     private:
         int size;
         short tiles[6][9];
+        short* right;
+        short* left;
+        short* front;
+        short* back;
+        short* top;
+        short* bottom;
+        bool correct; // set this flag to true after setting cube and validating all sides
+        bool solved; // flag set to true by updateIsSolved() if cube is in solved state
+
+        void updateIsSolved();
 
     public:
         cube(int _size);
+        bool setTiles(string order);
 };
 
 #endif
